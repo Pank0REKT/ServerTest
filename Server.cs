77031d884ecabc1b444995421ddddc4fa735bf5b
Pavel <pavelpan1517@gmail.com>
2023-07-18 21:27:10 +0300

    Создан класс поле и необходимые методы.

---
+++ Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    static class Server
+    {
+        private static int _count;
+
+        public static int GetCount()
+        {
+            return _count;
+        }
+
+        public static void AddToCount(int value)
+        {
+            _count += value;
+        }
+    }
+}
